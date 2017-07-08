fb66f33b7ffce979e7a1bf1491fff93bfcf44c78
Piotr <piotrdebicki92@gmail.com>
2017-07-08 12:37:02 -0400

    Add queries playground

--- server/server.js
+++ server/server.js
@@ -31,6 +31,20 @@ app.get('/todos', (req, res) => {
 });
 
 
+//
+// app.post('/users', (req, res) => {
+//   console.log(req.body);
+//   var user = new User({
+//     email: "piotdebick@com"
+//   });
+//
+//   todo.save().then((doc) => {
+//     res.send(doc);
+//   }, (e) => {
+//     res.status(400).send(e);
+//   });
+// });
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
