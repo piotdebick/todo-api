5d4facacf465831e9927abd567a785ccaba31e93
Piotr <piotrdebicki92@gmail.com>
2017-07-19 17:21:33 -0400

    ADd test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -294,5 +294,24 @@ describe('POST /users/login', () => {
         done();
       }).catch((e) => done(e));
     });
-  })
-})
+  });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).to.equal(0);
+        done();
+      }).catch((e) => done(e));
+    });
+
+  });
+});
